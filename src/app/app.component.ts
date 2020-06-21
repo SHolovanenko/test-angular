06d6ad76f58d74c478f08e3fd94e4310d0eb1434
Sergei Holovanenko <sergiobravos161@gmail.com>
2020-06-21 19:12:28 +0300

    init main structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Post } from './app.post';
+import { PostsService } from './app.postSrvice';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PostsService]
 })
-export class AppComponent {
-  title = 'test-angular';
+
+export class AppComponent implements OnInit {
+  posts: Array<Post>;
+
+  constructor(private postService: PostsService) {
+    //
+  }
+
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+  }
 }
---
+++ src/app/app.postSrvice.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Post } from './app.post';
+import { environment } from '../environments/environment';
+
+@Injectable()
+export class PostsService {
+
+    posts: Array<Post> = [];
+
+    constructor(private http: HttpClient) {
+        //
+    }
+
+    getPosts() : Array<Post> {
+        let url = 'https://gorest.co.in/public-api/posts?access-token='+environment.access_token;
+        let options = {
+            headers: new HttpHeaders({
+                'Authorization': 'Bearer ' + environment.access_token
+            })
+        };
+
+        this.http.get(url, options).subscribe((response) => {
+            response['result'].forEach(element => {
+                var post = new Post();
+                post.title = element.title;
+                post.body = element.body;
+
+                this.posts.push(post);
+            });
+        });
+
+        return this.posts;
+    }
+}
